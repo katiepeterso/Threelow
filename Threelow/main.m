9c075852c9425ad3657655e0f1dee2b39526097d
katiepeterso <katiepeterso@outlook.com>
2015-08-26 10:18:31 -0700

    Initial commit

---
+++ Threelow/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Katherine Peterson on 2015-08-26.
+//  Copyright (c) 2015 KatieExpatriated. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+⚀ ⚁ ⚂ ⚃ ⚄ ⚅
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
